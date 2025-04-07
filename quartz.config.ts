ceaf10758dfbffa87b10de5bce9be695defb3dac
Boyang Yan <yanboyang713@gmail.com>
2025-04-07 00:13:14 -0400

    update google analytics

--- quartz.config.ts
+++ quartz.config.ts
@@ -12,9 +12,7 @@ const config: QuartzConfig = {
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
-    analytics: {
-      provider: "plausible",
-    },
+    analytics: { provider: 'google', tagId: 'G-E7HDV72X78' },
     locale: "en-US",
     baseUrl: "yanboyang.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
